0592f945faee64066d8ad935a5cd604b9734a657
RhexNiebres <rhexniebres@gmail.com>
2024-11-22 15:33:58 -0800

    feat: add function playround to determine the winner of a round.

--- main.js
+++ main.js
@@ -16,7 +16,7 @@ console.log(ComputerChoice);
 function getHumanChoice() {
   const userInput = prompt(
     "Choose from, (rock, paper or scissors):"
-  ).toLowerCase();
+  ).toLowerCase(); // to be changed if not relevant
 
   if (
     userInput === "rock" ||
@@ -33,3 +33,57 @@ function getHumanChoice() {
 
 const humanChoice = getHumanChoice();
 console.log(`You choose: ${humanChoice}`);
+
+let humanScore = 0;
+let computerScore = 0;
+
+function playRound(humanChoice, computerChoice) {
+  humanChoice = humanChoice.toLowerCase();
+  computerChoice = computerChoice.toLowerCase();
+
+  if (humanChoice === computerChoice) {
+    console.log(
+      `It's a draw!${
+        humanChoice.charAt(0).toUpperCase() + humanChoice.slice(1)
+      }.`
+    );
+    return;
+  }
+  if (
+    (humanChoice === "rock" && computerChoice === "scissors") ||
+    (humanChoice === "scissors" && computerChoice === "paper") ||
+    (humanChoice === "paper" && computerChoice === "rock")
+  ) {
+    humanScore++;
+    console.log(
+      `You win! ${
+        humanChoice.charAt(0).toUpperCase() + humanChoice.slice(1)
+      }beats ${
+        computerChoice.charAt(0).toUpperCase() + computerChoice.slice(1)
+      }.`
+    );
+  } else if (
+    (humanChoice === "rock" && computerChoice === "paper") ||
+    (humanChoice === "scissors" && computerChoice === "rock") ||
+    (humanChoice === "paper" && computerChoice === "scissors")
+  ) {
+    computerScore++;
+    console.log(
+      `You lose! ${
+        computerChoice.charAt(0).toUpperCase() + computerChoice.slice(1)
+      } beats ${humanChoice.charAt(0).toUpperCase() + humanChoice.slice(1)}.`
+    );
+  } else {
+    console.log(
+      `Invalid choice: ${humanChoice}. Please choose rock, paper, or scissors.`
+    );
+    return;
+  }
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
+
+function playGAME() {}
